787e60abc948d5b7db4a56a35d3d29ecd93bc042
Ruslan Karabalin <karabalin112@gmail.com>
2024-04-18 09:20:38 +0600

    fix: some troubles

--- docker-demo_db.session.sql
+++ docker-demo_db.session.sql
@@ -1,6 +1,7 @@
-SELECT l1.locality_name,
-    l1.locality_id,
-    l2.locality_id,
-    l2.locality_name
+SELECT l1.locality_name as l1_name,
+    l1.locality_id as l1_id,
+    l2.locality_id as l2_id,
+    l2.locality_name as l2_name
 FROM localities as l1
-    join localities as l2 on l1.locality_id <> l2.locality_id;
+    join localities as l2 on l1.locality_id < l2.locality_id
+    AND l1.locality_name <> l2.locality_name;
--- lab2.sql
+++ lab2.sql
@@ -92,3 +92,11 @@ SELECT l1.locality_name,
     l2.locality_name
 FROM localities as l1
     join localities as l2 on l1.locality_id <> l2.locality_id;
+-- self join without duplicate pairs
+SELECT l1.locality_name as l1_name,
+    l1.locality_id as l1_id,
+    l2.locality_id as l2_id,
+    l2.locality_name as l2_name
+FROM localities as l1
+    join localities as l2 on l1.locality_id < l2.locality_id
+    AND l1.locality_name <> l2.locality_name;
