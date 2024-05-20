f8c8520936d60288a525dc80100ca3f17b327439
Ruslan Karabalin <karabalin112@gmail.com>
2024-05-20 21:11:38 +0600

    feat: start 4.4

--- labs/lab4.sql
+++ labs/lab4.sql
@@ -168,3 +168,78 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 select * from random_test();
+---------------------------------------------------------------
+-- 4.4
+---------------------------------------------------------------
+;
+CREATE OR REPLACE FUNCTION generate_and_analyze_data(num_records INT)
+RETURNS TABLE (
+    dataset TEXT,
+    mean DOUBLE PRECISION,
+    stddev DOUBLE PRECISION,
+    median DOUBLE PRECISION
+) AS $$
+DECLARE
+    avg_a DOUBLE PRECISION;
+    min_a INT;
+    max_a INT;
+    r RECORD;
+BEGIN
+    CREATE TEMP TABLE IF NOT EXISTS data (
+        id SERIAL PRIMARY KEY,
+        a INT
+    );
+
+    TRUNCATE TABLE data;
+
+    INSERT INTO data (a)
+    SELECT floor(random() * 100 + 1)::int
+    FROM generate_series(1, num_records);
+
+    SELECT AVG(a) INTO avg_a FROM data;
+
+    SELECT MIN(a), MAX(a) INTO min_a, max_a FROM data;
+
+    CREATE TEMP TABLE IF NOT EXISTS data_prime AS
+    SELECT id, 
+           CASE 
+               WHEN a > avg_a THEN min_a
+               ELSE max_a
+           END AS a_prime
+    FROM data;
+
+    TRUNCATE TABLE data_prime;
+
+    RAISE NOTICE 'DATA';
+    FOR r IN SELECT * FROM data LOOP
+        RAISE NOTICE '%', to_json(r);
+    END LOOP;
+    RAISE NOTICE 'DATA_PRIME';
+    FOR r IN SELECT * FROM data_prime LOOP
+        RAISE NOTICE '%', to_json(r);
+    END LOOP;
+
+    RETURN QUERY
+    WITH stats AS (
+        SELECT 
+            'data' AS dataset,
+            AVG(a)::double precision AS mean,
+            STDDEV(a)::double precision AS stddev,
+            PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY a)::double precision AS median
+        FROM data
+    ),
+    stats_prime AS (
+        SELECT 
+            'data_prime' AS dataset,
+            AVG(a_prime)::double precision AS mean,
+            STDDEV(a_prime)::double precision AS stddev,
+            PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY a_prime)::double precision AS median
+        FROM data_prime
+    )
+    SELECT * FROM stats
+    UNION ALL
+    SELECT * FROM stats_prime;
+
+END;
+$$ LANGUAGE plpgsql;
+select * from generate_and_analyze_data(10);
